0ab672dbe561a7a91bb5c2b9797702651efda151
Nagashree <nagashreemylaraiah@gmail.com>
2018-05-10 14:17:23 -0700

    Add functionality to: - Open Cards - Check for Match - Close Cards - Update moves - Check if won

--- js/app.js
+++ js/app.js
@@ -26,6 +26,72 @@ function shuffle(array) {
 }
 
 
+$(document).ready(function(){
+
+	var cardId;
+	var openCardList = [];
+	var matchedCardList = [];
+	var moves = 0;
+
+	$('.card').click(function(event){
+		cardId = String(event.target.id);
+		console.log (cardId);
+		openCard();
+		moves++;
+		updateMoves();
+		checkForMatch();
+	});
+
+	//Open the card clicked
+	function openCard(){
+		console.log("inside openCard");
+		document.getElementById(cardId).classList.add("show", "open");
+	};
+
+	//Close the unmatched card
+	function closeCard(element){
+		console.log("inside closeCard");
+		setTimeout(function(){
+			document.getElementById(element).classList.remove("show", "open");}, 500);
+	}
+
+	//Check if the cards match
+	function checkForMatch(){
+		openCardList.push(cardId);
+		if(!(openCardList.length<2)){
+			console.log(openCardList);
+			var card1 = String(openCardList[0]).slice(0,String(openCardList[0]).search("-"));
+			console.log(card1);
+			var card2 = String(openCardList[1]).slice(0,String(openCardList[1]).search("-"));
+			console.log(card2);
+			if(card1==card2){
+				matchedCardList.push(openCardList[0]);
+				matchedCardList.push(openCardList[1]);
+				console.log(matchedCardList);
+				if(matchedCardList.length==16){
+					console.log("you win");
+				}
+			}
+			else{
+				closeCard(String(openCardList[0]));
+				closeCard(String(openCardList[1]));
+			}
+		openCardList.splice(0,2);
+		console.log(openCardList);
+		}
+	};
+
+	//Update the number of moves
+	function updateMoves(){
+		document.querySelector("span.moves").textContent = String(moves);
+	};
+
+});
+
+
+
+
+
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
