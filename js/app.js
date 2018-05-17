4f62cf0c6725295ac4f5febd69be77052fc98688
Nagashree <nagashreemylaraiah@gmail.com>
2018-05-17 01:21:09 -0700

    Edits to ensure W3C validations are passed. Add documentation

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
 	let sec = document.querySelector(".sec");
 	
 	
-	for(i=0;i<classArray.length;i++){	/*Store the ids of all the cards in the deck in idArray */
+	for(let i=0;i<classArray.length;i++){	/*Store the ids of all the cards in the deck in idArray */
 		idArray[i]=classArray[i].getAttribute("id"); 
 	}	
 
@@ -29,10 +29,26 @@ $(document).ready(function(){
 
 /*..................Function definitions..............................*/
 
+	/*Refresh deck, Score Panel and all the variables*/
+	function refreshDeck(){
+		deckId.innerHTML=htmlTemp;
+		deckId.classList.remove("final");
+		shuffledDeck(idArray);
+		cardClick();
+		moves=0;
+		hour=0;
+		second=0;
+		minute=0;
+		matchedCardList=[];
+		html = " ";
+		updateMoves();
+		resetScore();
+		myVar = setInterval(myTimer,1000);/*Start the timer*/
+	}
+
 	/*Shuffle function from http://stackoverflow.com/a/2450976*/
 	function shuffle(array) {
 	    let currentIndex = array.length, temporaryValue, randomIndex;
-
 	    while (currentIndex !== 0) {
 	        randomIndex = Math.floor(Math.random() * currentIndex);
 	        currentIndex -= 1;
@@ -46,19 +62,18 @@ $(document).ready(function(){
 	/*Shuffle the cards in the deck*/
 	function shuffledDeck(cards){
 		let shuffledList= shuffle(cards);
-		let html="";
-		for(i=0;i<shuffledList.length;i++){	
-			html += `<li class="card" id="${shuffledList[i]}">${document.getElementById(shuffledList[i]).innerHTML}</li>`;
+		for(let shuffledCard of shuffledList){
+			html += `<li class="card" id="${shuffledCard}">${document.getElementById(shuffledCard).innerHTML}</li>`;
 		}
-		$(deck).empty();
-		$(deck).append(html);
-		clearInterval(myVar);/*Clear the timer*/
+		$(deck).empty(); /*Clear the deck*/
+		$(deck).append(html);/*Load shuffled cards*/
+		clearInterval(myVar);/*Clear the previous timer*/
 	}
 
 	/*Handle the  card click*/
 	function cardClick(){
 		$('.card').click(function(event){
-			cardId = String(event.target.id);
+			cardId = String(event.target.id);/*Store the id of the card clicked in cardId*/
 			openCard();
 			checkForMatch();
 		});
@@ -67,7 +82,7 @@ $(document).ready(function(){
 	/*Open the card clicked*/
 	function openCard(){
 		document.getElementById(cardId).classList.add("show", "open");
-	};
+	}
 
 	/*Close the unmatched card*/
 	function closeCard(element){
@@ -81,7 +96,7 @@ $(document).ready(function(){
 		if(openCardList.indexOf(cardId)==-1){ /*Proceed only if two different cards are clicked*/
 			openCardList.push(cardId); /*Add cards to the openCardist*/
 		
-			if(!(openCardList.length<2)){ /*Proceed only if there are atleast 2 cards to check for match*/
+			if(openCardList.length>=2){ /*Proceed only if there are atleast 2 cards to check for match*/
 				let card1 = String(openCardList[0]).slice(0,String(openCardList[0]).search("-")); /*Store details of first card in card1*/
 				let card2 = String(openCardList[1]).slice(0,String(openCardList[1]).search("-")); /*Store details of second card in card2*/
 				moves++; /*Increment the moves*/
@@ -89,38 +104,37 @@ $(document).ready(function(){
 				updateScore();
 		
 				if(card1==card2){ /*Check if the cards match and if they match, add them to a another list*/
-					matchedCardList.push(openCardList[0]);
-					matchedCardList.push(openCardList[1]);
-					openCardList.forEach(function(openCardList){
-						document.getElementById(openCardList).classList.add("match");
-					});					
+					for(let openCard of openCardList){
+						matchedCardList.push(openCard);
+					}
+					for(let matchedCard of matchedCardList){
+						document.getElementById(matchedCard).classList.add("match");/*Change the background color of matched cards*/
+					}				
 		
 					if(matchedCardList.length==16){ /*Check if all cards are matched*/
-						
 						updateScore();
-						displayMsg();
+						displayMsg(); 
 						clearInterval(myVar);/*Stop the timer when all cards are matched*/
 					}
-		
 				}
 				else{ /*Close cards if they dont match*/
-					closeCard(String(openCardList[0]));
-					closeCard(String(openCardList[1]));
+					for (let openCard of openCardList){
+						closeCard(String(openCard));
+					}
 				}
-		
 			openCardList.splice(0,2); /*Remove the unmatched cards from the openCardList*/
 			}
 		}
-	};
+	}
 
-	/*Check if the cards match*/
+	/*Update the no of Moves on the Score panel*/
 	function updateMoves(){
 		document.querySelector("span.moves").textContent = String(moves);
-	};
+	}
 
 	/*Highlight the stars based on Score*/
 	function updateScore(){
-		if (moves<=10){
+		if (moves<=15){
 			score = 3;
 		}
 		else if (moves<=20){
@@ -129,32 +143,16 @@ $(document).ready(function(){
 		else{
 			score = 1;
 		}
-		for(i=score;i<3;i++){
+		for(let i=score;i<3;i++){
 			document.getElementById(starScore[2-i].getAttribute("id")).classList.remove("highlight");
 		}
 	}
 
 	/*Reset the score*/
 	function resetScore(){
-		for(i=0;i<starScore.length;i++){
+		for(let i=0;i<starScore.length;i++){
 			document.getElementById(starScore[i].getAttribute("id")).classList.add("highlight");
 		}
-	}
-
-	/*Refresh deck and Score Panel*/
-	function refreshDeck(){
-		deckId.innerHTML=htmlTemp;
-		deckId.classList.remove("final");
-		shuffledDeck(idArray);
-		cardClick();
-		moves=0;
-		hour=0;
-		second=0;
-		minute=0;
-		matchedCardList=[];
-		updateMoves();
-		resetScore();
-		myVar = setInterval(myTimer,1000);/*Start the timer*/
 	}
 
 	/*Compute and Display the time elapsed*/
@@ -172,24 +170,23 @@ $(document).ready(function(){
 				hour++;
 			}
 		}
-
 		sec.textContent = ((second<10)?"0"+second:second);
 		min.textContent = ((minute<10)?"0"+minute:minute);
 		hr.textContent = ((hour<10)?"0"+hour:hour);
-	};
+	}
 
 	/*Display Message on Completion*/
 	function displayMsg(){
-		$(deck).empty();
 		deckId.classList.add("final");
-		
-		deckId.innerHTML = 
+		$(deck).empty();
+			
+		deckId.innerHTML = /*Update the deck to show the Congratulatory message upon matching all the cards*/
 		`<div class="win">
 			<p class="msg">Congratulations! You Won!</p>
 			<p>With ${moves} moves and ${score} stars<p>
-			<p>Total time taken: ${hour} hr ${minute} mins ${second} sec</p>
-			<button class="playAgain" type="button" class="btn btn btn-primary">Play again</button>
-		</div>`
+			<p>Total time taken: ${hour} hr ${minute} min ${second} sec</p>
+			<button class="playAgain" type="button" class="btn btn-primary">Play again</button>
+		</div>`;
 
 		$('.playAgain').click(function(){	/*Reshuffle the deck when the player wants to play again*/
 			refreshDeck();
